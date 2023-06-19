4a11f41b15d9ef5bd2e1ea1d93cf05ee6b12f639
Fatma NABLI <nabli.isigk@gmail.com>
2023-06-19 10:50:32 +0200

    link logo to home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 function Header(){
     return(
         <header>
-            <img className ="img-logo" src={logo} alt="logo kasa" />
+            <NavLink to="/"><img className ="img-logo" src={logo} alt="logo kasa" /></NavLink>
             <nav>
                 <ul>
                     <li><NavLink to="/">Accueil</NavLink></li>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import MyRouter from './MyRouter.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
